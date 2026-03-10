7a83fd387fe9b8f82481096907b62f4d0e139ff1
Piotr Kowal <127453125+Powal504@users.noreply.github.com>
2026-03-10 13:14:08 +0100

    Initial commit

---
+++ src/main/java/org/example/kurki/KurkiApplication.java
@@ -0,0 +1,13 @@
+package org.example.kurki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kurki/KurkiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kurki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
